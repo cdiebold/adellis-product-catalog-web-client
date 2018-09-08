7fb4f6f29d111a2c170bf1cc35ba1a6d112efa82
Chris Diebold <cdiebold2012@gmail.com>
2018-09-07 22:39:45 -0400

    added bootstrap sass and basic navbar component

---
+++ src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="comment-title">Title</label>
+        <input type="text" class="form-control" id="comment-title">
+      </div>
+      <div class="form-group">
+        <label for="comment-details">Message</label>
+        <textarea rows="15" class="form-control" id="comment-details"></textarea>
+      </div>
+      <button class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
